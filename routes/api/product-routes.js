fa572c2318129bb4c011985a6d9ee48a77f0119f
Eder Ramirez <edher.fca@gmail.com>
2022-04-21 22:15:50 -0500

    Route Product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  const products = await Product.findAll({
+    include: [{ model: Category }, { model: Tag }],
+	});
+	res.status(200).json(products);
 });
+
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  const product = await Product.findOne(req.params.id, {
+    include: [{ model: Category }, { model: Tag }],
+	});
+	res.status(200).json(product); 
 });
 
 // create new product
@@ -91,6 +100,21 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+try {
+ const product= await  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  });
+      if (!product) {
+        res.status(404).json({ message: 'No Product found with this id' });
+        return;
+      }
+
+      res.status(200).json(product);
+    } catch (err) {
+      res.status(500).json(err);
+    }
 });
 
 module.exports = router;
